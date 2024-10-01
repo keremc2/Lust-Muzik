413c394a2a9a8b82910d7ca729bfd41bde74b6a5
keremc2 <lustkerobusiness@gmail.com>
2024-10-01 21:17:17 +0300

    Add files via upload

---
+++ deneme/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta');
+
+    ctaButton.addEventListener('click', () => {
+        alert('Keşfetmeye başlayın!');
+    });
+});
